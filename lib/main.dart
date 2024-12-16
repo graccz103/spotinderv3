0ddb04b78b211aa04d122ff05747645b6f28e965
graccz103 <149792016+graccz103@users.noreply.github.com>
2024-12-16 22:46:51 +0100

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'spotify_service.dart'; // Pamiętaj o zaimportowaniu nowego pliku serwisowego
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,11 +33,35 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late Future<Map<String, dynamic>> artistData;
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  String? _currentPreviewUrl;
 
   @override
   void initState() {
     super.initState();
-    artistData = SpotifyService().getArtistData('4Z8W4fKeB5YxbusRsdQVPb'); // Przykładowe ID artysty
+    artistData = SpotifyService().getRandomArtist(); // Pobieranie losowego artysty
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  void _playPreview(String url) async {
+    if (_currentPreviewUrl == url) {
+      // Jeśli ten sam podgląd jest odtwarzany, zatrzymaj odtwarzanie
+      await _audioPlayer.stop();
+      setState(() {
+        _currentPreviewUrl = null;
+      });
+    } else {
+      // Odtwórz nowy podgląd
+      await _audioPlayer.play(UrlSource(url));
+      setState(() {
+        _currentPreviewUrl = url;
+      });
+    }
   }
 
   @override
@@ -50,21 +75,79 @@ class _MyHomePageState extends State<MyHomePage> {
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             if (snapshot.hasError) {
-              return Text("Error: ${snapshot.error}");
+              return Center(child: Text("Error: ${snapshot.error}"));
             }
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('Name: ${snapshot.data?['name'] ?? 'Unknown'}'),
-                  snapshot.data?['images'].isNotEmpty
-                      ? Image.network(snapshot.data?['images'][0]['url'])
-                      : SizedBox(),
-                ],
-              ),
-            );
+            if (snapshot.hasData && snapshot.data != null) {
+              var artist = snapshot.data!;
+              var artistName = artist['name'] ?? 'Unknown';
+              var imageUrl = artist['images'] != null && artist['images'].isNotEmpty
+                  ? artist['images'][0]['url']
+                  : null;
+              var albumName = artist['latest_album']?['name'] ?? 'Unknown';
+              var topTrack = artist['top_track']?['name'] ?? 'Unknown';
+              var previewUrl = artist['top_track']?['preview_url'];
+              var spotifyUrl = artist['top_track']?['spotify_url']; // Dodano link do Spotify
+              var followers = artist['followers']?['total'] ?? 0;
+
+              return SingleChildScrollView(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Artist Name: $artistName',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      if (imageUrl != null)
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Image.network(
+                            imageUrl,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      Text(
+                        'Latest Album: $albumName',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Text(
+                          'Top Track: $topTrack',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      if (previewUrl != null)
+                        ElevatedButton(
+                          onPressed: () => _playPreview(previewUrl),
+                          child: Text(
+                            _currentPreviewUrl == previewUrl ? 'Stop Preview' : 'Play Preview',
+                          ),
+                        )
+                      else if (spotifyUrl != null)
+                        ElevatedButton(
+                          onPressed: () => openSpotifyLink(spotifyUrl), // Otwieranie linku do Spotify
+                          child: Text('Open in Spotify'),
+                        )
+                      else
+                        Text(
+                          'Preview not available for this track',
+                          style: TextStyle(fontSize: 16, color: Colors.red),
+                        ),
+                      Text(
+                        'Followers: $followers',
+                        style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            } else {
+              return Center(child: Text("No data available"));
+            }
           } else {
-            return CircularProgressIndicator();
+            return Center(child: CircularProgressIndicator());
           }
         },
       ),
--- lib/spotify_service.dart
+++ lib/spotify_service.dart
@@ -1,32 +1,138 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:math';
+import 'package:url_launcher/url_launcher.dart';
 
 class SpotifyService {
   final String clientId = '2ee60615c2704ff2a0f06e3f9207a296';
   final String clientSecret = 'f2a9203eacd349b3bc9bcc93200a0e28';
 
+  // Pobieranie tokena dostępu
   Future<String> _getAccessToken() async {
     var response = await http.post(
       Uri.parse('https://accounts.spotify.com/api/token'),
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: 'grant_type=client_credentials&client_id=$clientId&client_secret=$clientSecret',
+      body:
+      'grant_type=client_credentials&client_id=$clientId&client_secret=$clientSecret',
     );
 
     var data = jsonDecode(response.body);
     return data['access_token'];
   }
 
-  Future<Map<String, dynamic>> getArtistData(String artistId) async {
+  // Pobieranie danych artysty
+  Future<Map<String, dynamic>> getArtistData(Map<String, dynamic> artist) async {
+    String artistId = artist['id'];
     String accessToken = await _getAccessToken();
+
+    // Pobierz najnowszy album
+    var latestAlbum = await getLatestAlbum(artistId, accessToken);
+    artist['latest_album'] = latestAlbum.isNotEmpty
+        ? latestAlbum
+        : {'name': 'No album available'};
+
+    // Pobierz topowy utwór
+    var topTrack = await getTopTrack(artistId, accessToken);
+    artist['top_track'] = topTrack.isNotEmpty
+        ? topTrack
+        : {
+      'name': 'No track available',
+      'preview_url': null,
+      'spotify_url': null,
+    };
+
+    return artist;
+  }
+
+  // Pobieranie najnowszego albumu artysty
+  Future<Map<String, dynamic>> getLatestAlbum(
+      String artistId, String accessToken) async {
     var response = await http.get(
-      Uri.parse('https://api.spotify.com/v1/artists/$artistId'),
+      Uri.parse(
+          'https://api.spotify.com/v1/artists/$artistId/albums?include_groups=album&limit=1'),
       headers: {
         'Authorization': 'Bearer $accessToken',
       },
     );
 
-    return jsonDecode(response.body);
+    var albumData = jsonDecode(response.body);
+    if (albumData['items'] != null && albumData['items'].isNotEmpty) {
+      return albumData['items'][0];
+    } else {
+      return {};
+    }
+  }
+
+  // Pobieranie top utworu artysty
+  Future<Map<String, dynamic>> getTopTrack(
+      String artistId, String accessToken) async {
+    var response = await http.get(
+      Uri.parse(
+          'https://api.spotify.com/v1/artists/$artistId/top-tracks?market=US'),
+      headers: {
+        'Authorization': 'Bearer $accessToken',
+      },
+    );
+
+    var trackData = jsonDecode(response.body);
+    if (trackData['tracks'] != null && trackData['tracks'].isNotEmpty) {
+      var firstTrack = trackData['tracks'][0];
+      return {
+        'name': firstTrack['name'],
+        'preview_url': firstTrack['preview_url'],
+        'spotify_url': firstTrack['external_urls']['spotify'],
+      };
+    }
+    return {};
+  }
+
+  // Pobieranie losowego artysty
+  Future<Map<String, dynamic>> getRandomArtist() async {
+    String accessToken = await _getAccessToken();
+    String randomQuery = _getRandomQuery();
+
+    // Wyszukaj artystę na podstawie losowego ciągu znaków
+    var response = await http.get(
+      Uri.parse(
+          'https://api.spotify.com/v1/search?q=$randomQuery&type=artist&limit=1'),
+      headers: {
+        'Authorization': 'Bearer $accessToken',
+      },
+    );
+
+    var searchData = jsonDecode(response.body);
+
+    if (searchData['artists'] != null &&
+        searchData['artists']['items'].isNotEmpty) {
+      var artist = searchData['artists']['items'][0];
+      if (artist.containsKey('id')) {
+        return getArtistData(artist);
+      }
+    }
+
+    // Ponów próbę, jeśli nie znaleziono artysty
+    return getRandomArtist();
+  }
+
+  // Generowanie losowego zapytania
+  String _getRandomQuery() {
+    const characters = 'abcdefghijklmnopqrstuvwxyz';
+    Random random = Random();
+
+    // Losowa litera lub dwie litery
+    int length = random.nextBool() ? 1 : 2;
+    return List.generate(length, (_) => characters[random.nextInt(characters.length)])
+        .join();
+  }
+}
+
+// Funkcja otwierająca link do Spotify
+Future<void> openSpotifyLink(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
