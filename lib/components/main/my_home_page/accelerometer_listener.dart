cf6d3b8e4c9d69370aafb261a10cee195cbb3d8d
graccz103 <149792016+graccz103@users.noreply.github.com>
2024-12-18 20:39:40 +0100

    Dobra oficial na master z poprawnym accelerometrem

--- lib/components/main/my_home_page/accelerometer_listener.dart
+++ lib/components/main/my_home_page/accelerometer_listener.dart
@@ -1,18 +1,36 @@
+import 'dart:async';
+
 import 'package:sensors_plus/sensors_plus.dart';
 
-void listenToAccelerometer(Function onTiltLeft, Function onTiltRight) {
-  bool isActionPerformed = false;
+class AccelerometerListener {
+  static StreamSubscription? _subscription;
+  static bool isListening = false;
 
-  accelerometerEvents.listen((event) {
-    if (!isActionPerformed) {
-      if (event.x < -7) {
-        isActionPerformed = true;
-        onTiltLeft();
-      } else if (event.x > 7) {
-        isActionPerformed = true;
-        onTiltRight();
+  static void startListening(Function onTiltLeft, Function onTiltRight) {
+    if (isListening) return;
+
+    bool isActionPerformed = false;
+    isListening = true;
+
+    _subscription = accelerometerEvents.listen((event) {
+      if (!isActionPerformed) {
+        if (event.x < -7) {
+          isActionPerformed = true;
+          onTiltLeft();
+        } else if (event.x > 7) {
+          isActionPerformed = true;
+          onTiltRight();
+        }
       }
-    }
-    if (event.x.abs() < 3) isActionPerformed = false;
-  });
+      if (event.x.abs() < 3) isActionPerformed = false;
+    });
+  }
+
+  static void stopListening() {
+    if (!isListening) return;
+
+    isListening = false;
+    _subscription?.cancel();
+    _subscription = null;
+  }
 }
